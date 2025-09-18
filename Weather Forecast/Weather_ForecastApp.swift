a1d3bada6fffd30adce840be9cd177091bebf8aa
tim <timothy.hart@gmail.com>
2025-09-18 15:44:35 -0400

    Initial Commit

---
+++ Weather Forecast/Weather_ForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_ForecastApp.swift
+//  Weather Forecast
+//
+//  Created by Timothy Hart on 9/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Weather_ForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
